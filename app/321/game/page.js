2a468251330cb69566f48eae5b718a9455eedf7f
Sergei <95417457+nowelflantier@users.noreply.github.com>
2023-07-19 00:30:18 +0200

    ajout de l'input sur la page game et début implémentation logique de jeu

--- app/321/game/page.js
+++ app/321/game/page.js
@@ -1,28 +1,114 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link';
-import styles from '../../styles.scss'
-import React, { useState, useEffect } from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../styles.scss";
+import React, { useState, useEffect } from "react";
 
 const Game = () => {
+  const darts = [
+    { id: 0, score: null },
+    { id: 1, score: null },
+    { id: 2, score: null },
+  ];
+  const [newCurrentScore, setNewCurrentScore] = useState("");
   const [players, setPlayers] = useState([]);
-  const [currentPlayer, setCurrentPlayer] = useState(1)
-
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [currentNewScore, setCurrentNewScore] = useState(0);
+  const [currentPlayerScore, setCurrentPlayerScore] = useState(
+    players[currentPlayer - 1]?.score * 1
+  );
+  const [currentDart, setCurrentDart] = useState(0);
+  const currentUserScore = players[currentPlayer- 1]?.score;
 
   useEffect(() => {
-    const storedPlayers = JSON.parse(localStorage.getItem('players')) || [];
+    const storedPlayers = JSON.parse(localStorage.getItem("players")) || [];
     setPlayers(storedPlayers);
-    
   }, [currentPlayer]);
+
+ 
+  const NextDart = () => {
+    const [newCurrentScore, setNewCurrentScore] = useState("");
+    
+    const handleAddScore = (newCurrentScore, currentDart) => {
+      darts[currentDart] = { id: currentDart, score: newCurrentScore };
+      console.log(darts);
+    };
+    const handleInputChange = (event) => {
+      setNewCurrentScore(event.target.value);
+    };
+    const updateDartScore = () => {
+      darts.score = newCurrentScore
+      console.log(darts)
+    }
+    const handleNewScore = () => {
+      
+      // updateDartScore()
+      handleAddScore(newCurrentScore, currentDart);
+      handleNextDart();
+      setNewCurrentScore("");
+      return newCurrentScore;
+    };
+    const handleNewTurn = () => {
+      handleNextPlayer();
+    };
+
+    return currentDart < 3 ? (
+      // affiche l'input et le bouton pour ajouter une valeur tant que la flèce actuelle est < 3
+      <div className="addScore">
+        <input
+          type="number"
+          placeholder="score"
+          onChange={handleInputChange}
+          value={newCurrentScore}
+          className="select"
+        />
+        <button
+          className="btn bottom"
+          value={newCurrentScore}
+          onClick={handleNewScore}
+        >
+          AddScore - {newCurrentScore}
+        </button>
+      </div>
+    ) : (
+      // sinon affiche le bouton pour passer au joueur suivant
+      <div className="addScore">
+        <button className="button" onClick={handleNewTurn}>
+          NextPlayer
+        </button>
+      </div>
+    );
+  };
+
+  const handleLastPlayer = () => {
+    setCurrentDart(0);
+    setCurrentPlayer(0);
+  };
+  const handleCurrentPlayer = () => {
+    setCurrentUserScore(currentUserScore);
+    setCurrentDart(0);
+    setCurrentPlayer(currentPlayer + 1);
+  };
+  const handleNextPlayer = () => {
+    currentPlayer === players.length - 1
+      ? handleLastPlayer()
+      : handleCurrentPlayer();
+  };
+
+  const handleNextDart = () => {
+    currentDart === 3
+      ? handleNextPlayer()
+      : setCurrentDart(currentDart + 1);
+  };
 
   return (
     <main className="main">
-    <div className="description">
-      <p>Jeu en cours de développement</p>
-      <div>powered by le S.</div>
-    </div>
-    <Image
+      <div className="description">
+        <p>Jeu en cours de développement</p>
+        <div>powered by le S.</div>
+      </div>
+      <Image
         className="logo"
         src="/dart-aim.svg"
         alt="Next.js Logo"
@@ -30,34 +116,50 @@ const Game = () => {
         height={87}
         priority
       />
-    <div className="active">
-      <h1 className="code">Player {currentPlayer}</h1>
-      <h2 className="code">{players[currentPlayer-1]?.name}</h2>
-      <h2 className="code">{players[currentPlayer-1]?.score}</h2>
-    </div>
-   
-    <div className="grid"> {players
-      .filter(player => player.id !== currentPlayer)
-      .map((player) => (
-        <div className="card" key={player.id}>
-          <h2>{player.id}. {player.name}</h2>
-          <p>Score : {player.score}</p>
+      <div className="active">
+        <h1 className="code">Player {currentPlayer}</h1>
+        <h2 className="code">{players[currentPlayer - 1]?.name}</h2>
+        <h2 className="code">{players[currentPlayer - 1]?.score}</h2>
+        <div className="addScore">
+        <NextDart
+      players={players}
+      currentPlayer={currentPlayer}
+      currentDart={currentDart}
+      currentUserScore={currentUserScore}
+      currentNewScore={currentNewScore}
+      handleNextDart={handleNextDart}
+      handleNextPlayer={handleNextPlayer}
+      darts={darts}
+    />
         </div>
-      ))}</div>
+      </div>
+
+      <div className="grid">
+        {" "}
+        {players
+          .filter((player) => player.id !== currentPlayer)
+          .map((player) => (
+            <div className="card" key={player.id}>
+              <h2>
+                {player.id}. {player.name}
+              </h2>
+              <p>Score : {player.score}</p>
+            </div>
+          ))}
+      </div>
       {/* La logique du jeu va ici */}
-    <div className="center container">
-      <p className="code">
-        work in progress
-        <br />
-      </p>
-      <Link href="/" className="bottom btn">
-        <p>Back home</p>
-      </Link>
-      <Link href="/321/select-players" className="bottom btn">
-        <p>Back to the player selection</p>
-      </Link>
-    </div>
-   
+      <div className="center container">
+        <p className="code">
+          work in progress
+          <br />
+        </p>
+        <Link href="/" className="bottom btn">
+          <p>Back home</p>
+        </Link>
+        <Link href="/321/select-players" className="bottom btn">
+          <p>Back to the player selection</p>
+        </Link>
+      </div>
     </main>
   );
 };
