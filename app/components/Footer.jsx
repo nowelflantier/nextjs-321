88ff2edaf7eb489fa4e421cad92276947012b922
Sergei <95417457+nowelflantier@users.noreply.github.com>
2023-08-03 21:36:13 +0200

    amélioration gestion des infos du jeu hors des composants

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -9,17 +9,20 @@ const Footer = ({ buttons, selectedGame }) => {
     return null;
   }
   return (
-    
-
     <div className="center container">
-      {buttons.map((button, index) => (
-        <a key={index} href={`${button.path}`}>
-          <p className="bottom btn">{button.text}</p>
-        </a>
-      ))}
-    </div>
-    // </ContextDataLoader>
-
+    {buttons.map((button, index) => {
+      // Vérifiez si le bouton est null ou undefined avant d'essayer d'accéder à ses propriétés
+      if (button !== null && button !== undefined) {
+        return (
+          <a key={index} href={`${button.path}`}>
+            <p className="bottom btn">{button.text}</p>
+          </a>
+        );
+      }
+      // Si le bouton est null ou undefined, ne renvoyez rien
+      return null;
+    })}
+  </div>
   );
 };
 
